f87506e1d93ffc1e06511e0e03a3b8e890d826cf
Robin Mueller <muellerr.95@gmail.com>
2020-11-09 09:28:12 +0100

    Replaced merged layer with EfficientNetB0, changed input images size to 224x224

--- epidef_fun/epidef_model.py
+++ epidef_fun/epidef_model.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import Flatten, Dense, Conv2D
 from tensorflow.keras.layers import Dropout, BatchNormalization, Input, MaxPool2D
 from tensorflow.keras.backend import concatenate
 import tensorflow as tf
-
+from tensorflow.keras.applications import EfficientNetB0
 
 def layer1_multistream(res_x, res_y, num_cams, filter_num):
     """
@@ -43,28 +43,31 @@ def layer2_merged(res_x, res_y, filter_num, conv_depth):
     :return:
     """
     seq = Sequential()
-    for i in range(conv_depth):
-        # seq.add(MaxPool2D((2, 2), name=f'S2_MP{i}'))  # v Do strides instead of MaxPooling? v
-        seq.add(Conv2D(filter_num, (2, 2), strides=(2, 2),
-                       input_shape=(int(res_x/2**i), int(res_y/2**i), filter_num),
-                       padding='valid', name=f'S2_C1{i}', activation='relu'))
-        seq.add(Conv2D(filter_num, (2, 2), padding='same', name=f'S2_C2{i}', activation='relu'))
-        seq.add(BatchNormalization(axis=-1, name=f'S2_BN{i}'))
+    seq.add(Conv2D(3, (3, 3), padding='valid', name='ABC', activation='relu'))
+    seq.add(EfficientNetB0(include_top=True, weights=None, classes=3))
+    # seq = Sequential()
+    # for i in range(conv_depth):
+    #     # seq.add(MaxPool2D((2, 2), name=f'S2_MP{i}'))  # v Do strides instead of MaxPooling? v
+    #     seq.add(Conv2D(filter_num, (2, 2), strides=(2, 2),
+    #                    input_shape=(int(res_x/2**i), int(res_y/2**i), filter_num),
+    #                    padding='valid', name=f'S2_C1{i}', activation='relu'))
+    #     seq.add(Conv2D(filter_num, (2, 2), padding='same', name=f'S2_C2{i}', activation='relu'))
+    #     seq.add(BatchNormalization(axis=-1, name=f'S2_BN{i}'))
     return seq
 
 
-def layer3_last():
-    """
-    Last layer: Flatten - Dense - ReLU - Dense - Sigmoid
-
-    :return:
-    """
-    seq = Sequential()
-    seq.add(Flatten())
-    seq.add(Dense(256, activation='relu', name=f"S3_d1"))
-    seq.add(Dense(128, activation='relu', name=f"S3_d2"))
-    seq.add(Dense(3, activation='sigmoid', name=f"S3_dfinal"))
-    return seq
+# def layer3_last():
+#     """
+#     Last layer: Flatten - Dense - ReLU - Dense - Sigmoid
+#
+#     :return:
+#     """
+#     seq = Sequential()
+#     seq.add(Flatten())
+#     seq.add(Dense(256, activation='relu', name=f"S3_d1"))
+#     seq.add(Dense(128, activation='relu', name=f"S3_d2"))
+#     seq.add(Dense(3, activation='sigmoid', name=f"S3_dfinal"))
+#     return seq
 
 
 def define_epidef(sz_input1, sz_input2, view_n, conv_depth, filter_num):
@@ -90,10 +93,11 @@ def define_epidef(sz_input1, sz_input2, view_n, conv_depth, filter_num):
     mid_merged = concatenate([mid_vert, mid_hori])
 
     # Merged layer: MaxPool - Conv - ReLU - Conv - ReLU - BN
-    mid_merged_ = layer2_merged(sz_input1, sz_input2, 2*filter_num, conv_depth)(mid_merged)
-
+    # mid_merged_ = layer2_merged(sz_input1, sz_input2, 2*filter_num, conv_depth)(mid_merged)
+    mid_merged_ = layer2_merged(mid_merged)
     # Last Dense layer: Dense - ReLU - Dense
-    output = layer3_last()(mid_merged_)
+    # output = layer3_last()(mid_merged_)
+    output = mid_merged_
     model_512 = Model(inputs=[input_stack_vert, input_stack_hori], outputs=[output])
     METRICS = ['accuracy',
                tf.keras.metrics.Precision(name='precision'),
