a503306ab356e4174ef17a04e1417114df6cffe4
rgmueller <muellerr.95@gmail.com>
2020-11-16 16:25:00 +0100

    0002 for workshop images

--- epidef_fun/util.py
+++ epidef_fun/util.py
@@ -17,7 +17,7 @@ def get_list_IDs(lf_directory):
     error = 0
     list_IDs = []
     for path, subdirs, files in os.walk(lf_directory):
-        if '0001_Set0_Cam_003_img.png' in files:  # only add paths with images
+        if '0002_Set0_Cam_003_img.png' in files:  # only add paths with images
 
             list_IDs.append(path)
             if 'good' in path:
@@ -59,44 +59,44 @@ def load_lightfield_data(list_IDs, IMG_SIZE):
     labels = np.zeros((len(list_IDs)), np.int64)
     for i, lf in enumerate(list_IDs):
         # print(lf.split('\\')[-2:])
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_000_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_000_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 0, 6, :] = tmp[:, :, :3]  # rightmost image
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_001_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_001_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 0, 5, :] = tmp[:, :, :3]  # (R,G,B,alpha)
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_002_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_002_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 0, 4, :] = tmp[:, :, :3]
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_003_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_003_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 0, 3, :] = tmp[:, :, :3]  # center image
         features[i, :, :, 1, 3, :] = tmp[:, :, :3]
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_004_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_004_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 0, 2, :] = tmp[:, :, :3]
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_005_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_005_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 0, 1, :] = tmp[:, :, :3]
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_006_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_006_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 0, 0, :] = tmp[:, :, :3]  # leftmost image
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_007_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_007_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 1, 0, :] = tmp[:, :, :3]  # bottom image
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_008_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_008_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 1, 1, :] = tmp[:, :, :3]
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_009_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_009_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 1, 2, :] = tmp[:, :, :3]
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_010_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_010_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 1, 4, :] = tmp[:, :, :3]
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_011_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_011_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 1, 5, :] = tmp[:, :, :3]
-        tmp = np.float32(imageio.imread(f"{lf}\\0001_Set0_Cam_012_img.png"))
+        tmp = np.float32(imageio.imread(f"{lf}\\0002_Set0_Cam_012_img.png"))
         tmp = tf.image.resize(tmp, (IMG_SIZE, IMG_SIZE))
         features[i, :, :, 1, 6, :] = tmp[:, :, :3]  # top image
         del tmp
