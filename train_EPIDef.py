8f09fa88954ed909055b14802f0ce68e86b6c88a
Robin Mueller <muellerr.95@gmail.com>
2020-11-23 11:28:15 +0100

    Fixed warnings

--- train_EPIDef.py
+++ train_EPIDef.py
@@ -3,8 +3,6 @@ import datetime
 import numpy as np
 
 import tensorflow as tf
-from tensorflow import keras
-from epidef_fun.generate_traindata import generate_traindata, data_augmentation
 from epidef_fun.util import get_list_ids
 from epidef_fun.epidef_model import define_epidef
 from epidef_fun.DataGenerator import DataGenerator
@@ -40,7 +38,7 @@ if __name__ == '__main__':
     print("Loading lightfield paths...")
     # dir_lf_images = ("C:\\Users\\muell\\Google Drive\\University\\Master_Project"
     #                  + "\\data_storage\\lightfields")
-    dir_lf_images = ("C:\\Users\\muell\\Desktop\\blender_output_tmp")
+    dir_lf_images = "C:\\Users\\muell\\Desktop\\blender_output_tmp"
     list_IDs = get_list_ids(dir_lf_images)[:100]
 
     print("Done loading lightfield paths.")
@@ -48,25 +46,6 @@ if __name__ == '__main__':
     list_IDs_train, list_IDs_test = list_IDs[:fraction], list_IDs[fraction:]
 
     model = define_epidef(input_res, input_res, 7, model_filter_number)
-
-    # Load latest checkpoint
-    if load_weights:
-        list_name = os.listdir(directory_ckp)
-        if len(list_name) >= 1:
-            list1 = os.listdir(directory_ckp)
-            list_i = 0
-            for list1_tmp in list1:
-                if list1_tmp == 'checkpoint':
-                    list1[list_i] = 0
-                    list_i += 1
-                else:
-                    list1[list_i] = int(list1_tmp.split('_')[0][4:])
-                    list_i += 1
-            list1 = np.array(list1)
-            iter00 = list1[np.argmax(list1)] + 1
-            ckp_name = list_name[np.argmax(list1)].split('.hdf5')[0] + '.hdf5'
-            model.load_weights(directory_ckp + '\\' + ckp_name)
-            print(f"Network weights will be loaded from previous checkpoints {ckp_name}")
 
     # Write date & time
     # f1 = open(txt_name, 'a')
